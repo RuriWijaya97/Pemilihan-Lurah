4d658e83b9dbc91dadf8ea9314adfcc80ba58844
Ruri Wijaya <rury.wijaya@gmail.com>
2021-06-17 13:10:12 +0700

    initial commit

--- pemilihanLurah.js
+++ pemilihanLurah.js
@@ -1,5 +1,27 @@
 function cariLurahBaru(calons) {
-    // your code here
+    calons.sort((x, y) => x - y);
+
+    var bestStreak = 1;
+    var bestElem = calons[0];
+    var currentStreak = 1;
+    var currentElem = calons[0];
+
+    for (let i = 1; i < calons.length; i++) {
+        if (calons[i - 1] !== calons[i]) {
+            if (currentStreak > bestStreak) {
+                bestStreak = currentStreak;
+                bestElem = currentElem;
+            }
+
+            currentStreak = 0;
+            currentElem = calons[i];
+        }
+
+        currentStreak++;
+    }
+
+    return currentStreak > bestStreak ? currentElem : bestElem;
+
 }
 
 console.log(cariLurahBaru([1, 2, 3, 2, 1, 3, 2, 3])); // pemilihan ulang
